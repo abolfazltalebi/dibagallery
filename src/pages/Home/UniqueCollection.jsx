d005dc0b0cad6742e9be2553c524fa038dd6b9c3
Your Name <you@example.com>
2025-03-25 14:02:25 +0330

    add link to image UniqueCollection

--- src/pages/Home/UniqueCollection.jsx
+++ src/pages/Home/UniqueCollection.jsx
@@ -1,14 +1,15 @@
 import Bdesktop from '@/assets/images/banners/banner_desktop.webp';
 import Bmobile from '@/assets/images/banners/banner_mobile.webp';
+import { Link } from 'react-router-dom';
 export default function UniqueCollection() {
   return (
     <section className="container">
-      <div className="md:inline hidden">
+      <Link className="md:inline hidden">
         <img src={Bdesktop} alt="banner desktop" />
-      </div>
-      <div className=" md:hidden">
+      </Link>
+      <Link className=" md:hidden">
         <img src={Bmobile} alt="banner mobile" />
-      </div>
+      </Link>
     </section>
   );
 }
